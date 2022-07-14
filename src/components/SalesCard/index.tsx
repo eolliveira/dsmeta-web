8b25ed64f04509e3b7e46fb8173da2a24601d01b
Erick OLiveira <erick.oliveira9607@gmail.com>
2022-07-13 22:09:57 -0300

    useState

--- src/components/SalesCard/index.tsx
+++ src/components/SalesCard/index.tsx
@@ -1,14 +1,37 @@
-import FormControl from '../FormControl';
 import SalesTable from '../SalesTable';
 import './style.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
 
 function SalesCard() {
+
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
-            <FormControl/>
-            <FormControl/>
-            <SalesTable/>
+            <div className="dsmeta-form-control-container">
+                <DatePicker
+                    selected={minDate}
+                    onChange={(date: Date) => { setMinDate(date) }}
+                    className="dsmeta-form-control"
+                    dateFormat="dd/MM/yyyy"
+                />
+            </div>
+            <div className="dsmeta-form-control-container">
+                <DatePicker
+                    selected={maxDate}
+                    onChange={(date: Date) => { setMaxDate(date) }}
+                    className="dsmeta-form-control"
+                    dateFormat="dd/MM/yyyy"
+                />
+            </div>
+            <SalesTable />
         </div>
     );
 }
